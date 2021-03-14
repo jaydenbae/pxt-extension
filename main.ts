1bbd29cc460ce283538e7b8b385f00daa0ba2d43
jaydenbae <77905021+jaydenbae@users.noreply.github.com>
2021-03-14 11:34:28 +0900

    pxt.json, main.ts, test.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
-})
+namespace testBlock {
+    /**Show string hello */
+    //% blockId=showhellostring
+    //% block="showhello"
+    export function testBlock(){  
+       basic.showString("Hello!")
+    }
+}
